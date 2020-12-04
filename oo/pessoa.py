7d47bba5bbfdecd3e1d352d9d05fd9bb5cf64ec0
Rafael Faria <rafaelsanfaria@gmail.com>
2020-12-03 21:42:23 -0300

    adiciona metodo init com nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá ${id(self)}'
 
 
 if __name__ == '__main__':
-    pessoa = Pessoa()
+    pessoa = Pessoa('Rafael')
     print(Pessoa.cumprimentar(pessoa))
+    print(id(pessoa))
     print(pessoa.cumprimentar())
+    print(pessoa.nome)
+    pessoa.idade = 29
+    print(pessoa.idade)
