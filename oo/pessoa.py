94b2af0b4aea58a65fd5acbffe2365aaa27ae5f4
Rafael Faria <rafaelsanfaria@gmail.com>
2020-12-03 22:31:57 -0300

    adiciona loop para print dos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,7 +2,7 @@ class Pessoa:
     def __init__(self, *filhos, nome=None, idade=30):
         self.nome = nome
         self.idade = idade
-        self.filhos = filhos
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá ${id(self)}'
@@ -10,10 +10,12 @@ class Pessoa:
 
 if __name__ == '__main__':
     rafael = Pessoa(nome='Rafael', idade=10)
-    julia = Pessoa(rafael, 'Julia')
+    julia = Pessoa(rafael, nome='Julia')
     print(Pessoa.cumprimentar(rafael))
     print(id(rafael))
     print(rafael.cumprimentar())
     print(rafael.nome)
     print(rafael.idade)
-    print(julia.filhos)
+    for filho in julia.filhos:
+        print(filho.nome)
+
